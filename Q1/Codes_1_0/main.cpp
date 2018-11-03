3a67933d6dacf429e4f67e7e56f7228fddc88564
周远笛 <1094050886@qq.com>
2018-11-03 21:01:09 +0800

    Q1Q2代码部分完成，还差效果更佳的优化以及结果分析

--- Q1/Codes_1_0/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "HistogramEqualization.h"
-#include <iostream>
-using namespace std;
-
-int main() {
-	//HistogramEqualizationForGray histogramEqualizationForGray("Lena.bmp", "LenaGrayHE.bmp");
-	HistogramEqualizationForColor histogramEqualizationForColor("Lena.bmp", "LenaColorHE.bmp");
-}
---
+++ Q2/main.cpp
@@ -0,0 +1,10 @@
+#include "HistogramEqualization.h"
+#include "ColorTransfer.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	//HistogramEqualizationForGray histogramEqualizationForGray("Lena.bmp", "LenaGrayHE.bmp");
+	//HistogramEqualizationForColor histogramEqualizationForColor("Lena.bmp", "LenaColorHE.bmp");
+	ColorTransfer colorTransfer("4.bmp", "3.bmp");
+}
